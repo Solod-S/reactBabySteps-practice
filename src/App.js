0a3ebd5e075e0f3a907906315863d29fa17357e1
Solod Sergey <Solod098@gmail.com>
2022-09-27 19:48:19 +0300

    class, extends, constructor(), render(), setState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import Counter from './components/Counter/Counter';
-import Dropdown from './components/Dropdown/Dropdown';
+import Counter from './components/Counter';
+import Dropdown from './components/Dropdown/';
+import TodoList from './components/TodoList';
 import ColorPicker from './components/ColorPicker/ColorPicker';
+import initialTodoes from './components/TodoList/initialTodoes.json';
 const colorPickerOptions = [
   { label: 'red', color: '#F44336' },
   { label: 'green', color: '#4CAF50' },
@@ -11,14 +13,50 @@ const colorPickerOptions = [
   { label: 'indigo', color: '#3F51B5' },
 ];
 
-const App = () => (
-  <>
-    <h1>Состояние компонента</h1>
-    {/* <Counter initialValue={1} /> */}
+class App extends React.Component {
+  state = {
+    todos: initialTodoes,
+  };
+  deleteTodo = todosId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todos => todos.id !== todosId),
+    }));
+  };
+
+  render() {
+    const { todos } = this.state;
+    // const completedTodos = todos.filter(todos => todos.completed).length;
+    const completedTodos = todos.reduce(
+      (total, todos) => (todos.completed ? total + 1 : total),
+      0,
+    );
+
+    return (
+      <>
+        {/* <h1>Состояние компонента</h1>
+    <Counter initialValue={1} />
     <Dropdown />
-    <ColorPicker options={colorPickerOptions} />
-  </>
-);
+    <ColorPicker options={colorPickerOptions} /> */}
+        <div>
+          <p>Общее кол-во: {todos.length} </p>
+          <p>Кол-во оставшихся: {initialTodoes.length - todos.length} </p>
+          <p>Кол-во выполненных: {completedTodos} </p>
+        </div>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo}></TodoList>
+      </>
+    );
+  }
+}
+
+// const App = () => (
+//   <>
+//     {/* <h1>Состояние компонента</h1>
+//     <Counter initialValue={1} />
+//     <Dropdown />
+//     <ColorPicker options={colorPickerOptions} /> */}
+//     <TodoList></TodoList>
+//   </>
+// );
 export default App;
 //
 //
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,15 +1,67 @@
 import React from 'react';
 import './TodoList.css';
-
 const TodoList = ({ todos, onDeleteTodo }) => (
   <ul className="TodoList">
-    {todos.map(({ id, text }) => (
-      <li key={id} className="TodoList__item">
-        <p className="TodoList__text">{text}</p>
-        <button onClick={() => onDeleteTodo(id)}>Удалить</button>
-      </li>
-    ))}
+    {todos.map(({ id, text, completed }) => {
+      return (
+        <li className="TodoList__item" key={id}>
+          <p className="TodoList__text">{text}</p>
+          <button onClick={() => onDeleteTodo(id)}>Удалить</button>
+        </li>
+      );
+    })}
   </ul>
 );
 
 export default TodoList;
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// import React from 'react';
+// import './TodoList.css';
+
+// const TodoList = ({ todos, onDeleteTodo }) => (
+//   <ul className="TodoList">
+//     {todos.map(({ id, text }) => (
+//       <li key={id} className="TodoList__item">
+//         <p className="TodoList__text">{text}</p>
+//         <button onClick={() => onDeleteTodo(id)}>Удалить</button>
+//       </li>
+//     ))}
+//   </ul>
+// );
+
+// export default TodoList;
